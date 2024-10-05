730091a18077cf6d8d1416c68576089b3254346e
eflaatten <elliotflaatten@live.com>
2024-10-05 11:29:24 -0500

    added middleware to profile routes

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { changeProfilePicture, changeUsername, getProfileInfo } = require("../controllers/profile");
+const { authenticateToken } = require("../middleware/authenticate");
 const router = express.Router();
 
-router.post('/change-profile-picture', changeProfilePicture);
-router.post('/change-username', changeUsername);
-router.get('/get-profile', getProfileInfo);
+router.post('/change-profile-picture', authenticateToken, changeProfilePicture);
+router.post('/change-username', authenticateToken, changeUsername);
+router.get('/get-profile', authenticateToken, getProfileInfo);
 
 module.exports = router;
