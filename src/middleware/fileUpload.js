f11618e05fd4a852ca7b45753674052935772ff5
eflaatten <elliotflaatten@live.com>
2024-10-06 22:02:39 -0500

    multer issue

--- src/middleware/fileUpload.js
+++ src/middleware/fileUpload.js
@@ -5,6 +5,6 @@ const storage = multer.memoryStorage(); // Store the file in memory
 const upload = multer({
   storage: storage,
   limits: { fileSize: 5 * 1024 * 1024 }, // Limit to 5MB
-});
+}).single("profile_picture"); // Specify the name of the field expected
 
 module.exports = upload;
