8f974562ad8eae7dbbf0ee0506172b47387ef9ed
eflaatten <elliotflaatten@live.com>
2024-10-08 20:27:19 -0500

    added profile route, email

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { changeProfilePicture, changeUsername, getProfileInfo } = require("../controllers/profile");
+const { changeEmail, changeUsername, getProfileInfo } = require("../controllers/profile");
 const { authenticateToken } = require("../middleware/authenticate");
 const router = express.Router();
 
+router.post('/change-email', authenticateToken, changeEmail);
 router.post('/change-username', authenticateToken, changeUsername);
 router.get('/get-profile', authenticateToken, getProfileInfo);
 
