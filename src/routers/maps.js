e96ac4d4ba8af02cb72ad0e556ae169b0c834774
eflaatten <elliotflaatten@live.com>
2024-10-23 19:03:58 -0500

    changed req type pois

--- src/routers/maps.js
+++ src/routers/maps.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { fetchRoutes, fetchPois, geocodeLocation } = require("../controllers/maps");
 
 router.get('/routes', fetchRoutes);
-router.get('/pois', fetchPois);
+router.post('/pois', fetchPois);
 router.get('/geocode', geocodeLocation);
 
 module.exports = router;
