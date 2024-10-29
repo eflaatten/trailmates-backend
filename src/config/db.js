1acaa0695374d7cb89e7eb7f8b4268a136c6658c
eflaatten <elliotflaatten@live.com>
2024-10-28 21:48:11 -0500

    redeploy new db setup

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,8 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  connectTimeout: 10000,
 });
 
 const promisePool = pool.promise();
